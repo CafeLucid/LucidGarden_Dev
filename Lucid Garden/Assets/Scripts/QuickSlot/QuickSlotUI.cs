e83ec7419778940d93c96db4c26862f18a17ed02
jHyuk22 <jhlee4938@gmail.com>
2024-05-25 20:22:06 +0900

    feat: 퀵슬롯 시스템 구현

--- Lucid Garden/Assets/Scripts/QuickSlot/QuickSlotUI.cs
+++ Lucid Garden/Assets/Scripts/QuickSlot/QuickSlotUI.cs
@@ -6,51 +6,10 @@ using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 
-public class QuickSlotUI : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+public class QuickSlotUI : MonoBehaviour
 {
     public GameObject quickSlotUI;
     public TextMeshProUGUI[] itemCounts;
-    public List<QuickSlotItem> itemPrefabs = new List<QuickSlotItem>();
-
-    private GameObject draggingItem;
-    private RectTransform draggingPlane;
-
-
-    public void OnBeginDrag(PointerEventData eventData)
-    {
-        // 드래그 시작 시 아이템 아이콘을 생성
-        int slotIndex = transform.GetSiblingIndex();
-        draggingItem = Instantiate(itemPrefabs[slotIndex + 1].itemPrefab, transform);
-        draggingItem.transform.SetAsLastSibling();
-        draggingPlane = transform as RectTransform;
-    }
-
-    public void OnDrag(PointerEventData eventData)
-    {
-        if (draggingItem != null)
-        {
-            SetDraggedPosition(eventData);
-        }
-    }
-
-    public void OnEndDrag(PointerEventData eventData)
-    {
-        if (draggingItem != null)
-        {
-            Destroy(draggingItem);
-            if (eventData.pointerEnter != null)
-            {
-                //말풍선에 드래그
-            }
-        }
-    }
-
-    private void SetDraggedPosition(PointerEventData eventData)
-    {
-        if (RectTransformUtility.ScreenPointToWorldPointInRectangle(draggingPlane, eventData.position, eventData.pressEventCamera, out Vector3 globalMousePos))
-        {
-            draggingItem.transform.position = globalMousePos;
-        }
-    }
+    public List<DragSlot> dragSlots;
 }
 
