efd07b9d478209f543465e5f250681918bb2dc31
jHyuk22 <jhlee4938@gmail.com>
2024-05-26 05:25:12 +0900

    feat: 사운드 추가

---
+++ Lucid Garden/Assets/Scripts/SoundController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class SoundController : MonoBehaviour
+{
+    public AudioSource cow;
+    public AudioSource roaster;
+    public AudioSource click;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            click.Play();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (!cow.isPlaying && !roaster.isPlaying)
+        {
+            int random = Random.Range(0, 1000);
+            if (random == 1) cow.Play();
+            else if (random == 2) roaster.Play();
+        }
+    }
+}
