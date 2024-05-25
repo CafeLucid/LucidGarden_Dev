479d232640db1ca22867c953c31a54d052cc2c32
jHyuk22 <jhlee4938@gmail.com>
2024-05-25 15:34:25 +0900

    feat: 퀵슬롯 아이템 프리팹 생성

---
+++ Lucid Garden/Assets/Scripts/QuickSlot/QuickSlotItem.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class QuickSlotItem : MonoBehaviour
+{
+    public GameObject itemPrefab;
+    public int id;
+    public int index;
+}
